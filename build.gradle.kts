8e0cda670e00c355772b3619acc25724765d07ec
raphael-goetz <raphael.goetz.7@gmail.com>
2025-04-07 20:02:36 +0200

    feat: added GitHub publishing configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,14 +17,53 @@ java {
     withSourcesJar()
     withJavadocJar()
 
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_23
+    targetCompatibility = JavaVersion.VERSION_23
 }
 
 publishing {
     publications {
-        register<MavenPublication>("mavenJava") {
-            from(components["java"])
+        create<MavenPublication>("mavenJava") {
+
+            groupId = "studio.o7"
+            artifactId = "octopus-sdk"
+            version = "0.0.1"
+
+            pom {
+                name = "octopus-sdk"
+                url = "https://github.com/o7studios/octopus-sdk"
+                description = "The official o7studios octopus-sdk"
+
+                developers {
+                    developer {
+                        id = "raphael-goetz"
+                        name = "Raphael Goetz"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:git://github.com/o7studios/octopus-sdk.git"
+                    developerConnection = "scm:git:git@https://github.com/o7studios/octopus-sdk.git"
+                    url = "https://github.com/o7studios/octopus-sdk"
+                    tag = "HEAD"
+                }
+
+                ciManagement {
+                    system = "GitHub Actions"
+                    url = "https://github.com/o7studios/octopus-sdk/actions"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GithubPackages"
+            url = uri("https://github.com/o7studios/octopus-sdk")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
         }
     }
 }
